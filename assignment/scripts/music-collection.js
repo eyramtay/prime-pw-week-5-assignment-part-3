07ee946d724d43117a32c33829ee3f539af6c7f6
eyramtay <71682389+eyramtay@users.noreply.github.com>
2021-03-23 14:34:12 -0500

    Added album objects to collection variable

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -1,1 +1,49 @@
 console.log('***** Music Collection *****')
+
+let collection = [
+  {
+        title: 'Take Care',
+        artist: 'Drake',
+        yearPublished: 2011,
+    },
+    {
+      title: 'After Hours',
+      artist: 'The Weeknd',
+      yearPublished: 2020,
+    },
+    {
+      title: 'Changes',
+      artist: 'Justin Bieber',
+      yearPublished: 2020,
+    },
+    {
+      title: 'When It\'s All Said And Done...Take Time',
+      artist: 'GIVEON',
+      yearPublished: 2021,
+    },
+    {
+      title: 'The 20/20 Experience',
+      artist: 'Justin Timberlake',
+      yearPublished: 2013,
+    },
+    {
+      title: 'JESUS IS KING',
+      artist: 'Kanye West',
+      yearPublished: 2019,
+    },
+];
+
+function addToCollection (title, artist, yearPublished) {
+  return collection;
+  console.log('My music collection contains: ', collection);
+}
+
+console.log('My favorite albums are: ', collection);
+
+// function showCollection ([]) {
+//   console.log(showCollection());
+// }
+
+function findByArtist (artist) {
+  
+}
